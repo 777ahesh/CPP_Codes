40f156121b02e7e9815deb9d26abbfb877775140
Mahesh Gaikwad <gaikwadmahesh777g@gmail.com>
2021-11-04 15:32:58 +0530

    Started with DSA Learning Series - Contest 1 Solutions

---
+++ Codechef/DSA Learning Series - Contest 1/TEST.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long d;
+	while (true){
+	   cin>>d;
+	   if(d == 42){
+	       break;
+	   }
+	   else cout<<d<<endl;
+	}
+	return 0; 
+}
