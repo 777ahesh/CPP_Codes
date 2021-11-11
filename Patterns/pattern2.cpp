26049ede08fceff39822a2c67721a6305b3573bf
Mahesh Gaikwad <gaikwadmahesh777g@gmail.com>
2021-11-11 22:45:59 +0530

    Started with patterns

---
+++ Patterns/pattern2.cpp
@@ -0,0 +1,30 @@
+// pattern 2 : 
+
+ /*
+
+    1  2  3  4  5 
+    2  4  6  8  10  
+    3  6  9  12 15
+    4  8  12 16 20
+    5  10 15 20 25
+
+ */
+
+#include<iostream>
+using namespace std;
+int main(){
+  
+  // User Inputs : 
+  
+  int n;
+  cin>>n;
+  
+  for(int i=1; i<=n; i++){
+      for(int j=1; j<=n; j++){
+          cout<<i*j<<"  ";
+      }cout<<endl;
+  }
+
+ return 0;
+}
+
